6be59201738a868f464f9c5d9c0c1a16c24de6ec
David Dumas <david@dumas.io>
2022-02-02 11:34:35 -0600

    Debugging example

---
+++ samplecode/debugging/baddec.py
@@ -0,0 +1,19 @@
+"""Decorator example with an error (for debugging)"""
+# MCS 275 - David Dumas
+
+def twice(f):
+    """Decorator that replaces a function with a version
+    that runs twice"""
+    def inner(*args,**kwargs):
+        """Call a certain function twice, returning the
+        second result."""
+        f(*args,**kwargs)
+        return f(*args,**kwargs)
+    return inner
+
+def hello():
+   """Print greeting"""
+   print("Hello everyone!")
+
+hello_twice = twice(hello())
+hello_twice()
